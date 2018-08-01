f22f9e4a78a52e1e58d18793687ede8743782e16
Felipe Amalfi Lima <31046813+FelipeAmalfi@users.noreply.github.com>
2018-07-31 21:54:29 -0300

    0.0.15 toObject complete
    Updated: GenericEntity/toObject()
    Added: GenericEntity/toList()

--- app/src/main/java/br/com/tercom/Entity/GenericEntity.java
+++ app/src/main/java/br/com/tercom/Entity/GenericEntity.java
@@ -1,9 +1,11 @@
 package br.com.tercom.Entity;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import br.com.tercom.Annotation.BindObject;
@@ -11,17 +13,13 @@ import br.com.tercom.Annotation.BindObject;
 
 public class GenericEntity
 {
-    public void toObject(String json) {
+    public Object toObject(String json) {
         try {
             JSONObject jObj = new JSONObject(json);
-            //TODO()
-//            try{
-//                jObj = new JSONObject().getJSONObject("result");
-//            }
-//            catch(Exception e)
-//            {
-//
-//            }
+
+            if(jObj.has("result"))
+                jObj = jObj.getJSONObject("result");
+
             jObj = jObj.getJSONObject("attributes");
             Iterator<String> keys = jObj.keys();
             Field field;
@@ -31,16 +29,14 @@ public class GenericEntity
                 if(field.isAnnotationPresent(BindObject.class))
                 {
                     BindObject bo = field.getAnnotation(BindObject.class);
-                    if(bo.type() == BindObject.TYPE.LIST)
-                    {
 
+                    if(bo.type() == BindObject.TYPE.OBJECT)
+                    {
+                        field.set(this,toObject(jObj.get(bo.value()).toString()));
                     }
                     else
                     {
-                        //ANTES - objType era um atributo de BindObject
-//                        Object obj = bo.objType().newInstance();
-//                        Method m = bo.objType().getDeclaredMethod("toObject", String.class);
-//                        m.invoke(obj, jObj.getJSONObject());
+                        field.set(this, toList(jObj.get(bo.value()).toString(),field.getType()));
                     }
                 }
                 else
@@ -49,12 +45,38 @@ public class GenericEntity
                     field.set(this, jObj.get(field.getName()));
                 }
             }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            } catch (NoSuchFieldException e) {
-                e.printStackTrace();
+
+            return this;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return null;
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public <T> ArrayList<T> toList(String json,Class<T> selectedClass) {
+        try {
+
+            JSONObject jObj = new JSONObject(json);
+            JSONArray jsonArray = jObj.getJSONArray("elements");
+            ArrayList<T> values = new ArrayList<>();
+
+            for(int i = 0; i< jsonArray.length();i++)
+            {
+                values.add(selectedClass.cast(toObject(jsonArray.getString(i))));
             }
+
+            return values;
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
