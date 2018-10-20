fa01f5fb8ae679d58c2a726200d192d72b8ae05e
gmargutti <gustavo.margutti@hotmail.com>
2018-10-20 02:09:28 -0300

    0.0.28 - Work in Progress: WebServices ProductValue

--- app/src/main/java/br/com/tercom/Adapter/ProductValueAdapter.java
+++ app/src/main/java/br/com/tercom/Adapter/ProductValueAdapter.java
@@ -1,5 +1,4 @@
 package br.com.tercom.Adapter;
-
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -7,54 +6,62 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import java.util.ArrayList;
-
+import java.util.Locale;
 import br.com.tercom.Entity.ProductValue;
+import br.com.tercom.Interface.RecyclerViewOnClickListenerHack;
 import br.com.tercom.R;
-
 public class ProductValueAdapter extends RecyclerView.Adapter<ProductValueAdapter.ViewHolder> {
     private LayoutInflater layoutInflater;
     private ArrayList<ProductValue> productValues;
     private Context context;
-
-    public ProductValueAdapter(Context context, ArrayList<ProductValue> productValues)
-    {
-        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        this.productValues = productValues;
-        this.context = context;
+    private RecyclerViewOnClickListenerHack mRecyclerViewOnClickListenerHack;
+    public void setmRecyclerViewOnClickListenerHack(RecyclerViewOnClickListenerHack mRecyclerViewOnClickListenerHack) {
+        this.mRecyclerViewOnClickListenerHack = mRecyclerViewOnClickListenerHack;
     }
-
-    @NonNull
+    public ProductValueAdapter(Context c, ArrayList<ProductValue> providers) {
+        layoutInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.productValues = providers;
+        this.context = c;
+    }
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View v = layoutInflater.inflate(R.layout.item_productvalue, parent, false);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = layoutInflater.inflate(R.layout.item_provider,parent,false);
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
-
     @Override
-    public void onBindViewHolder(@NonNull ProductValueAdapter.ViewHolder holder, int position) {
-        holder.txtNome.setText(productValues.get(position).getName());
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.productName.setText(productValues.get(position).getName());
+        holder.productPackage.setText(String.format(Locale.getDefault(),"%s %s",productValues.get(position).getAmount(),productValues.get(position).getPackage().getName()));
+        holder.productPrice.setText(String.format(Locale.getDefault(),"R$ %.2f",productValues.get(position).getPrice()));
+        holder.productManufacturer.setText(productValues.get(position).getName());
+        holder.productProvider.setText(productValues.get(position).getProvider().getFantasyName());
     }
-
     @Override
     public int getItemCount() {
         return productValues.size();
     }
-
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        public TextView txtNome;
-
+        public TextView productName;
+        public TextView productPackage;
+        public TextView productPrice;
+        public TextView productManufacturer;
+        public TextView productProvider;
         public ViewHolder(View itemView) {
             super(itemView);
-            txtNome = itemView.findViewById(R.id.txtNome_ProductValue);
+            productName = itemView.findViewById(R.id.productName);
+            productPackage = itemView.findViewById(R.id.productPackage);
+            productPrice = itemView.findViewById(R.id.productPrice);
+            productManufacturer = itemView.findViewById(R.id.productManufacturer);
+            productProvider = itemView.findViewById(R.id.productProvider);
             itemView.setOnClickListener(this);
         }
-
         @Override
-        public void onClick(View view) {
-
+        public void onClick(View v) {
+            if(mRecyclerViewOnClickListenerHack != null){
+                mRecyclerViewOnClickListenerHack.onClickListener(v,getPosition());
+            }
         }
     }
 }
