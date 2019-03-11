d307c7ab7385508c100dd19dbce914622ed63660
Felipe Amalfi Lima <31046813+FelipeAmalfi@users.noreply.github.com>
2019-03-11 20:56:46 -0300

    0.0.66 Adicionado método para receber produtos e serviços de um pedido.

--- app/src/main/java/br/com/tercom/Boundary/Activity/InicializedOrderListActivity.java
+++ app/src/main/java/br/com/tercom/Boundary/Activity/InicializedOrderListActivity.java
@@ -1,19 +1,28 @@
 package br.com.tercom.Boundary.Activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Looper;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 
 import br.com.tercom.Adapter.DetailOrderListAdapter;
 import br.com.tercom.Boundary.BoundaryUtil.AbstractAppCompatActivity;
+import br.com.tercom.Control.OrderItemControl;
+import br.com.tercom.Entity.ApiResponse;
 import br.com.tercom.Entity.Manufacture;
 import br.com.tercom.Entity.OrderItemProduct;
+import br.com.tercom.Entity.OrderItemProductList;
 import br.com.tercom.Entity.OrderItemService;
+import br.com.tercom.Entity.OrderItemServiceList;
+import br.com.tercom.Entity.OrderRequest;
 import br.com.tercom.Entity.Product;
 import br.com.tercom.Entity.Provider;
 import br.com.tercom.Entity.Services;
@@ -26,11 +35,8 @@ import butterknife.OnClick;
 
 public class InicializedOrderListActivity extends AbstractAppCompatActivity {
 
-    private ArrayList<iNewOrderItem> list;
-    private Product product;
-    private Provider provider;
-    private Manufacture manufacturer;
-
+    private GetAllProductListTask getAllProductListTask;
+    private OrderRequest orderRequest;
     @BindView(R.id.rv_InicializedOrderDetail)
     RecyclerView rv_InicializedOrderDetail;
 
@@ -43,47 +49,73 @@ public class InicializedOrderListActivity extends AbstractAppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicialized_order_detail);
         ButterKnife.bind(this);
-        //createToolbar();
-        populate();
-        DetailOrderListAdapter detailOrderListAdapter = new DetailOrderListAdapter(this, list);
+        createToolbar();
+        try{
+            orderRequest = new Gson().fromJson(getIntent().getExtras().getString("order"),OrderRequest.class);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        initGetAllProducts();
+
+    }
+
+    private void createNewOrderList(ArrayList<? extends iNewOrderItem> list) {
+        DetailOrderListAdapter detailOrderListAdapter= new DetailOrderListAdapter(this, list);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
-        rv_InicializedOrderDetail.setLayoutManager(layoutManager);
-        rv_InicializedOrderDetail.setAdapter(detailOrderListAdapter);
         detailOrderListAdapter.setmRecyclerViewOnClickListenerHack(new RecyclerViewOnClickListenerHack() {
             @Override
             public void onClickListener(View view, int position) {
-                createIntentAbs(OrderInsertValueActivity.class);
+
             }
         });
+        rv_InicializedOrderDetail.setLayoutManager(layoutManager);
+        rv_InicializedOrderDetail.setAdapter(detailOrderListAdapter);
     }
 
-    public void populate() {
-        list = new ArrayList<iNewOrderItem>();
-        OrderItemProduct orderItemProduct = new OrderItemProduct();
-        OrderItemService orderItemService = new OrderItemService();
-        Product product = new Product();
-        Services services = new Services();
-        Provider provider = new Provider();
-        Manufacture manufacture = new Manufacture();
 
-        product.setName("Produto Teste");
-        services.setName("Serviço Teste");
-        provider.setFantasyName("Fornecedor Teste");
-        manufacture.setFantasyName("Fabricante Teste");
+    private void initGetAllProducts(){
+        if(getAllProductListTask == null || getAllProductListTask.getStatus() != AsyncTask.Status.RUNNING){
+            getAllProductListTask = new GetAllProductListTask(orderRequest.getId());
+            getAllProductListTask.execute();
+        }
+    }
 
-        orderItemProduct.setId(1);
-        orderItemProduct.setProduct(product);
-        orderItemProduct.setProvider(provider);
-        orderItemProduct.setManufacturer(manufacture);
-        orderItemProduct.setObservations("teste");
 
-        orderItemService.setId(1);
-        orderItemService.setService(services);
-        orderItemService.setProvider(provider);
-        orderItemService.setObservations("teste");
+    private class GetAllProductListTask extends AsyncTask<Void, Void, Void> {
+        private ArrayList<iNewOrderItem> list;
 
-        list.add(orderItemProduct);
-        list.add(orderItemService);
+        private ApiResponse<OrderItemProductList> apiResponseProduct;
+        private ApiResponse<OrderItemServiceList> apiResponseService;
+        private int id;
+
+        public GetAllProductListTask (int id){
+            list = new ArrayList<>();
+            this.id = id;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            if(Looper.myLooper() == null) {
+                Looper.prepare();
+            }
+            OrderItemControl orderItemControl = new OrderItemControl(InicializedOrderListActivity.this);
+            apiResponseProduct = orderItemControl.getAllProducts(id);
+            apiResponseService = orderItemControl.getAllServices(id);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            if(apiResponseProduct.getStatusBoolean()){
+                list.addAll(apiResponseProduct.getResult().getList());
+            }
+            if(apiResponseService.getStatusBoolean()){
+                list.addAll(apiResponseService.getResult().getList());
+            }
+            if(list.size() > 0)
+                createNewOrderList(list);
+
+        }
     }
 
 }
